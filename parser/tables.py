bf7b54159668b11a4408cc24b5b293ace12998ba
kthnyt <keithtnyati@gmail.com>
2020-06-22 20:50:44 +0200

    Refactor tables.py

--- parser/tables.py
+++ parser/tables.py
@@ -1,37 +1,5 @@
 import os
 
-from pandas.compat._optional import import_optional_dependency
-
-_IMPORTS = False
-_HAS_BS4 = False
-_HAS_PANDAS = False
-_HAS_HTML5LIB = False
-
-
-def _importers():
-    # import things we need
-    # but make this done on a first use basis
-
-    global _IMPORTS
-    if _IMPORTS:
-        return
-
-    global _HAS_BS4, _HAS_PANDAS, _HAS_HTML5LIB
-    bs4 = import_optional_dependency("bs4", raise_on_missing=False, on_version="ignore")
-    _HAS_BS4 = bs4 is not None
-
-    pandas = import_optional_dependency(
-        "pandas", raise_on_missing=False, on_version="ignore"
-    )
-    _HAS_PANDAS = pandas is not None
-
-    html5lib = import_optional_dependency(
-        "html5lib", raise_on_missing=False, on_version="ignore"
-    )
-    _HAS_HTML5LIB = html5lib is not None
-
-    _IMPORTS = True
-
 def _read(obj):
     """
     Try to read from a url, file or string.
@@ -83,13 +51,21 @@ class Table:
         self.io = io
         self.encoding = encoding
 
-    # def _decompose_xmlns_link(self, doc):
-    #     from bs4 import SoupStrainer
+    def _decompose_xmlns_link(self, doc):
+        from bs4 import SoupStrainer
 
-    #     _strainer = SoupStrainer('link')
-    #     element_name  = _strainer.name
+        _strainer = SoupStrainer('link')
+        element_name  = _strainer.name
 
-    #     links = doc.find_all(element_name, attrs=attrs)
+        links = doc.find_all(element_name)
+
+        if not links:
+            raise ValueError("No links found")
+        
+        for link in links:
+            link.decompose()
+
+        return doc
 
     def _setup_build_doc(self):
         raw_text = _read(self.io)
@@ -101,5 +77,4 @@ class Table:
         from bs4 import BeautifulSoup
         return BeautifulSoup(self._setup_build_doc(), features='html5lib',
                                 from_encoding=self.encoding)
-
 
